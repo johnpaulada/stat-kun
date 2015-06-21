a92498d0c0dc26b8bf74e5a4fb1ac3e596eae741
John Paul Ada <johnpaulada@jp-mini.local.gap>
2015-06-21 17:00:21 +0800

    Modal added.

--- www/js/src/stat-kun.ind.t.controller.js
+++ www/js/src/stat-kun.ind.t.controller.js
@@ -1,5 +1,5 @@
 (function(angular) {
-	angular.module('statkun').controller("IndTController", ['$scope', 'IndependentT', IndTController]);
+	angular.module('statkun').controller("IndTController", ['$scope', '$ionicModal', 'IndependentT', IndTController]);
 	
 	/**
 	 * Controls the Independent Samples t-test process
@@ -7,7 +7,7 @@
 	 * @params  {Object} $scope
 	 * @returns {undefined}
 	 */
-	function IndTController($scope, IndependentT) {
+	function IndTController($scope, $ionicModal, IndependentT) {
 		
 		// Initialize
 		$scope.dataset = [];
@@ -35,6 +35,40 @@
 			summary.y = 'Experimental group: M = ' + $scope.results.yStats.mean + ', SD = ' + $scope.results.yStats.sd;
 			
 			conclusion = 'The difference between the two groups is ' + ($scope.results.sig ? '' : 'not') + 'significant t(' + $scope.results.df + ') = ' + $scope.results.tCalc + ', p ' + ($scope.results.sig ? '<' : '>') + ' 0.05.';
+			
+			$scope.modalTitle = 'Results!';
+			$scope.modalContent = conclusion;
+			$scope.openModal();
 		}
+		
+		$ionicModal.fromTemplateUrl('my-modal.html', {
+		    scope: $scope,
+		    animation: 'slide-in-up'
+		}).then(function(modal) {
+		    $scope.modal = modal;
+		});
+		  
+		$scope.openModal = function() {
+			$scope.modal.show();
+		};
+		
+		$scope.closeModal = function() {
+		    $scope.modal.hide();
+		};
+		
+		//Cleanup the modal when we're done with it!
+		$scope.$on('$destroy', function() {
+		    $scope.modal.remove();
+		});
+		
+		// Execute action on hide modal
+		$scope.$on('modal.hidden', function() {
+		    // Execute action
+		});
+		  
+		// Execute action on remove modal
+		$scope.$on('modal.removed', function() {
+		  // Execute action
+		});
 	}
 })(window.angular);
